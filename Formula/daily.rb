6613116572b380aed2dce30f8efa0387e35c829d
Diego <dtellez95@gmail.com>
2024-04-27 17:12:47 +0100

    update sha and version tag

--- Formula/daily.rb
+++ Formula/daily.rb
@@ -1,8 +1,8 @@
 class Daily < Formula
     desc "CLI tool to track your daily tasks to help you share with your team during daily standup"
     homepage "https://github.com/dtellz/daily"
-    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "63a2b6e1217c38d2a060d8642b4f3113ec73e006a642013e11236da64f104b16"
+    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.2.tar.gz"
+    sha256 "7e6042b24c4e0b2c02cc19b735e5a56d7126ed506dc4559ab396423e3723db38"
     license "MIT"
   
     def install
