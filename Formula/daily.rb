3493b403eec5a94e937c1dbb8f732b089d8781b0
Diego <dtellez95@gmail.com>
2024-04-27 17:38:07 +0100

    fix typo in src url

--- Formula/daily.rb
+++ Formula/daily.rb
@@ -1,7 +1,7 @@
 class Daily < Formula
     desc "CLI tool to track your daily tasks to help you share with your team during daily standup"
     homepage "https://github.com/dtellz/daily"
-    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.3.tar.gz"
+    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v.0.0.3.tar.gz"
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     license "MIT"
   
