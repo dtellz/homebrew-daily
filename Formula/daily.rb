8271f2e4d62a7fb0e643d59fbfd4bd2533202a40
Diego <dtellez95@gmail.com>
2024-04-27 17:03:27 +0100

    update first release url and calculated sha

--- Formula/daily.rb
+++ Formula/daily.rb
@@ -1,8 +1,8 @@
 class Daily < Formula
     desc "CLI tool to track your daily tasks to help you share with your team during daily standup"
     homepage "https://github.com/dtellz/daily"
-    url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.2.tar.gz"
-    sha256 "3073dac8c03a513ff38661c9cc8dab792c1f59ce6477235609f106bfe80d3d5c"
+    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "63a2b6e1217c38d2a060d8642b4f3113ec73e006a642013e11236da64f104b16"
     license "MIT"
   
     def install
