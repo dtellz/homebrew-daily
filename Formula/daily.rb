834bf692dbacc661e7b478f4dc753534768adb08
Diego Tellez <dtellez95@gmail.com>
2024-05-31 18:30:01 +0200

    bump

--- Formula/daily.rb
+++ Formula/daily.rb
@@ -1,8 +1,8 @@
 class Daily < Formula
     desc "CLI tool to track your daily tasks to help you share with your team during daily standup"
     homepage "https://github.com/dtellz/daily"
-    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.9.tar.gz"
-    sha256 "59a6943fd84ecb24caa8034ae178063e7f720e5a7b13a1553606243fc275c675"
+    url "https://github.com/dtellz/homebrew-daily/archive/refs/tags/v0.0.10.tar.gz"
+    sha256 "1f7a6cadc848963bff07b8d7a0df819e99776d93935c4f80b6e2a3205d745846"
     license "MIT"
   
     def install
